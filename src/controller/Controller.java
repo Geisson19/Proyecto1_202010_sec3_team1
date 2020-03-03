e093217021e7085186329191f14b0ab962208b76
Alejandro Gonzalez <60270608+lagonzalezd@users.noreply.github.com>
2020-03-02 21:59:06 -0500

    Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.Scanner;
 
+import model.data_structures.Queue;
 import model.logic.Comparendo;
 import model.data_structures.Queue;
 import model.logic.Modelo;
@@ -14,6 +15,14 @@ public class Controller {
 
     private View view;
 
+    /**
+     * Crear la vista y el modelo del proyecto
+     */
+    public Controller() {
+        view = new View();
+        modelo = new Modelo();
+    }
+
     public void run() {
         Scanner lector = new Scanner(System.in);
         boolean fin = false;
@@ -25,49 +34,38 @@ public class Controller {
 
             switch (option) {
                 case 1:
+                    modelo = new Modelo();
 
-                    try {
+                    long start = System.currentTimeMillis();
+                    modelo.cargarDatos();
+                    long end = System.currentTimeMillis();
 
-                        long start = System.currentTimeMillis();
-                        Queue<Comparendo> cola = modelo.cargarDatosPila();
+                    Queue<Comparendo> cola = modelo.darDatosC();
 
-                        long end = System.currentTimeMillis();
 
-                        // Tiempo de carga
-                        view.printMessage("Tiempo de carga (seg): " + (end - start) / 1000.0);
-                        // Total datos cargados
-                        view.printMessage("Total datos cargados: " + cola.darTamano());
-                        // Mayor objectID
-                        view.printMessage("Comparendo con el mayor Object Id: " + modelo.mayorObjectID());
-                        // Zona Minimax
-                        view.printMessage("Zona Minimax: " + " Menor Latitud= " + modelo.menorLatitud() +
-                                " Menor Longitud= " + modelo.menorLongitud() + " Mayor Latitud= " + modelo.mayorLatitud() + " Mayor Longitud= " + modelo.mayorLongitud() + "\n");
+                    // Tiempo de carga
+                    view.printMessage("Tiempo de carga (seg): " + (end - start) / 1000.0);
 
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    // Total datos cargados
+                    view.printMessage("Total datos cargados: " + cola.darTamano());
+                    // Mayor objectID
+                    view.printMessage("Comparendo con el mayor Object Id: " + modelo.mayorObjectID());
+                    // Zona Minimax
+                    view.printMessage("Zona Minimax: " + " Menor Latitud= " + modelo.menorLatitud() +
+                            " Menor Longitud= " + modelo.menorLongitud() + " Mayor Latitud= " + modelo.mayorLatitud() + " Mayor Longitud= " + modelo.mayorLongitud() + "\n");
 
                     break;
                 case 2:
-                    view.printMessage(" Ingrese la localidad en la que desea buscar.");
-                    String loc = lector.next();
-                    try {
-                        view.printMessage("x "+modelo.buscarPorLocalidad(loc));
-                    } catch (Exception e) {
-                        e.getMessage();
-                    }
 
                     break;
+                case 3:
 
 
-				case 3:
-					
+                default:
+                    view.printMessage("--------- \n Opcion Invalida !! \n---------");
+                    break;
+            }
+        }
 
-				default: 
-					view.printMessage("--------- \n Opcion Invalida !! \n---------");
-					break;
-			}
-		}
-		
-	}	
+    }
 }
